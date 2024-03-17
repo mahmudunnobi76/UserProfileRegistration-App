70b51a5336a2bc1cd8936154d5eea92a5ae5a573
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2024-03-17 16:42:02 +0600

    Add All the Requirements

---
+++ app/src/main/java/org/freedu/userprofileregistration/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.freedu.userprofileregistration
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listButton = findViewById(R.id.profileListBtn)
+
+        listButton.setOnClickListener {
+            startActivity(Intent(this, ProfileListActivity::class.java))
+            finish()
+        }
+    }
+}
